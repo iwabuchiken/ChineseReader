f78c485c59eab28a5278111c7faea521e7d2dd26
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-24 09:05:40 +0900

    B=master
    [d]
    1. git => up to "g a ."
    
    [n]
    1. "g a remote ..."

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChineseReader::Application.config.secret_token = '8cf661ea17e46f762f5b1cb8b5866ffd58b044f42237c501ee340f6602f8d36a31aa91f6af9f15c47c65cbde753e891db1a95577c54f79ab18eb3b49aea39070'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ChineseReader::Application.config.session_store :cookie_store, key: '_ChineseReader_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ChineseReader::Application.config.session_store :active_record_store
