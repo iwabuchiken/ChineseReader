b60fd4a2e580e199d2ee075eebe16da5fd26fb85
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-28 23:04:21 +0900

    B=master
    [d]
    1. Changed: Text:text => type=text, limit=nil

--- db/migrate/20120528133656_text_change_text_length.rb
+++
@@ -1,9 +0,0 @@
-class TextChangeTextLength < ActiveRecord::Migration
-  def up
-    change_column :texts, :text, :text, {:limit => 1000}
-  end
-
-  def down
-    change_column :texts, :text, :text, {:limit => 255}
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120528133656) do
+ActiveRecord::Schema.define(:version => 20120528140120) do
 
   create_table "texts", :force => true do |t|
-    t.text     "text",       :limit => 1000
+    t.text     "text"
     t.string   "url"
     t.string   "genre"
-    t.datetime "created_at",                 :null => false
-    t.datetime "updated_at",                 :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
     t.string   "title"
   end
 
