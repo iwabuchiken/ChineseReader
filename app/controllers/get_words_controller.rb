2ce6f2bada544cf33ffc966775a7a292871075c3
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-28 19:55:20 +0900

    B=master
    [d]
    1. New: get_words_controller.rb
    
    [n]
    1. Add links (see memo.)

--- app/controllers/get_words_controller.rb
+++ app/controllers/get_words_controller.rb
@@ -1,7 +1,19 @@
 class GetWordsController < ApplicationController
   def main
+    # Find certain word sets
+    # @word_set = WordSet.find(:all, :conditions => {:text_id => 2}, :limit => 3)
+  
+    # render "main"
+    @message = "main"
+    
+    @word_set = WordSet.first()
+    
+    @word_set_special = WordSet.where('text_id = ?', 2)
+    
   end
 
   def show
+    # @data = params[:text_id]
+    @word_set_special = WordSet.where('text_id = ?', Integer(params[:text_id]))
   end
 end
