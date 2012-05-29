7395e9007e7a887fe9ad4df0dc43fe8f75f80639
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-05-29 16:09:45 +0900

    B=B1
    [d]
    1. Added: controller => "dictionary"

--- app/controllers/dictionary_controller.rb
+++ app/controllers/dictionary_controller.rb
@@ -1,8 +1,22 @@
 class DictionaryController < ApplicationController
   def main
-    render "dictionary"
+    # render "dictionary"
   end
 
   def show
+    # Get param
+    # if params['chinese'].to_s.length != 0
+    if params['chinese'].to_s.length == 0
+      @japanese = "NO DATA"
+    else
+      # @japanese = params['chinese'].class.to_s
+      # @japanese = params['chinese'].class.to_s
+      # @japanese = params['chinese']
+      # params['chinese']
+      # @japanese = WordSet.where('ch = ?', params['chinese'])
+      # @japanese_words = WordSet.where('ch = ?', params['chinese'])
+      @word_sets = WordSet.where('ch = ?', params['chinese'])
+    end
+     
   end
 end
