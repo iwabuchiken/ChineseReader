8ad0b6b66f2f1b63515ee89a35701eff492b1a6d
iwabuchiken <iwabuchi.k.2010@gmail.com>
2012-06-04 08:36:11 +0900

    B=master
    Just committing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,18 @@ class ApplicationController < ActionController::Base
   def set_controller_and_action_names
     @current_controller = controller_name
     @current_action     = action_name
+    
+    # # User agent
+    @user_agent = request.env["HTTP_USER_AGENT"]
+    # # @user_agent = request.env["REMOTE_USER"]
+    # @user_agent = request.env["REQUEST_URI"]
+#     
+    # if @user_agent == nil
+      # @user_agent = "nil"
+    # end
+    
+    # request.env
+    # @request_env = request.env
+    
   end
 end
